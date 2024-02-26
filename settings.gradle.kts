80662426e035ec4e485779dd4f8dabe40a87086d
sergiherradorITB <sergi.herrador.7e7@itb.cat>
2024-02-26 08:23:58 +0100

    Pues eso

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
-rootProject.name = "m06-Sergi_Herrador-EA1-PersistenciaenfitxersJSON"
+rootProject.name = "m06-Sergi_Herrador_Elkin-David-EA1-PersistenciaenfitxersJSON"
 
